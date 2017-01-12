b622a06c775c77fe059fd7291d969a10c48accc9
mclosos <mclosos@gmail.com>
2017-01-12 22:35:37 +0300

    added abstract authentication data function

---
+++ auth_data.py
@@ -0,0 +1,8 @@
+HOSTS = {"184.253.11.40": ["admin",b"Ft6gGT"], "localhost":["adm", b"1234"]}
+
+
+def auth_data(ip_addr):
+    """
+    Abstract function which return login and password
+    """
+    return HOSTS[ip_addr]
