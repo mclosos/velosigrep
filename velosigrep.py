d5215c3576c95be16a52bd3009e0ee445eeff7a4
mclosos <mclosos@gmail.com>
2017-01-12 22:02:38 +0300

    added coments

--- velosigrep.py
+++ velosigrep.py
@@ -1,12 +1,10 @@
-import subprocess
 import os
-import pty
 from services import *
 import pexpect
 
 HOST="localhost"
 line_number = 20
-
+# create connection with ssh, open log with cat
 connection = pexpect.spawn('ssh ' + services[HOST][0] + '@' + HOST + ' cat ' + filename)
 authentication = connection.expect(['Are you sure you want to continue connecting','password:', pexpect.EOF])
 if authentication == 0:
@@ -19,7 +17,7 @@ elif authentication == 2:
     pass
 
 output = connection.before.decode(encoding='UTF-8')
-
+# print -100 and +100 lines from which was sent
 for i, line in enumerate(output.split(os.linesep)):
     if (line_number-100) <= i <= (line_number+100):
         print(i, line)
