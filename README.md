5f29cca72f9f98051c6ed731bdee3017cc201318
mclosos <mclosos@gmail.com>
2017-01-15 23:29:18 +0300

    print_specified_lines

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # velosigrep
 
-Script connects to remote host, read specified file, print -100 and +100 lines from specified line.
+Script connects to remote host, find and read (first which found) specified file, print line with specified index (first which found), print -100 and +100 lines from it.
 
 Be sure you have port 22 opened on target host.
 
 <pre>
 python == 3.4.3
-pexpect==4.2.1
+paramiko==2.1.1
 </pre>
 
 importing authentication data from auth_data.py
 
-Usage: python3 velosigrep.py --ip=localhost --path=/logs/ --file=logfile --line=150
+Usage: python3 velosigrep.py --ip=183.15.19.4 --path=/logs/ --index=8440 --file=logfile
--- velosigrep.py
+++ velosigrep.py
@@ -1,4 +1,5 @@
 import io
+import os
 import paramiko
 import argparse
 from auth_data import auth_data
@@ -9,7 +10,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--ip', help='ip-address of remote host, example:127.0.0.1')
 parser.add_argument('--path', help='path to the log file, example: /var/log/', default='/var/log/')
 parser.add_argument('--file', help='name of log file, example: syslog')
-parser.add_argument('--line', help='number of target line', default='0')
 parser.add_argument('--index', help='index of log')
 args = parser.parse_args()
 # create a connection with ssh, open log with cat
@@ -17,9 +17,8 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=args.ip, username=auth_data(args.ip)[0], password=auth_data(args.ip)[1], port=22)
 stdin, stdout, stderr = client.exec_command('find ' + args.path + ' -name ' + args.file)
-found = stdout.read().decode("utf-8").split('\n')
+found = stdout.read().decode('utf-8').split(os.linesep)
 log_file_path = found[0]
-print(log_file_path)
 client.close()
 # get the file
 t = paramiko.Transport((args.ip, 22))
@@ -30,15 +29,26 @@ try:
 finally:
     sftp_client.close()
 
-with io.open('./logfile', encoding='utf-8') as file:
+# Find and print line with specified index
+with io.open("./logfile", encoding='utf-8') as file:
+    line_number = 0
     for l in file:
-        if args.index in l:
-            print(l, end="\n\n")
-            print('#########################')
-"""
-# print -100 and +100 lines from which was sent
-for i, line in enumerate(output.split(os.linesep)):
-    if (int(args.line)-100) <= i <= (int(args.line)+100):
-        print(i, line)
-"""
+        line_number += 1
+        if args.index in l:            
+            print("#########################\n")
+            print(line_number, l, end="\n")
+            print("#########################\n")
+            break
+
+line_numbers_to_print = [line for line in range(int(line_number)-100, int(line_number)+100)]
+
+# Find and print -100 and +100 lines
+with io.open("./logfile", encoding='utf-8') as file:
+    line_number = 0
+    for l in file:
+        line_number += 1   
+        if line_number in line_numbers_to_print:
+            print(line_number, l)
+
+os.remove("./logfile")
 
